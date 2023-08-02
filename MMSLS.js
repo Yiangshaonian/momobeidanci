886018f1c1ab1093b279ed5650198e687bb23878
Yiangshaonian <57665590+Yiangshaonian@users.noreply.github.com>
2023-08-02 20:54:37 +0800

    Update MMSLS.js

--- MMSLS.js
+++ MMSLS.js
@@ -6,9 +6,9 @@
 
 const MMSLS = [
     
-    "https://www.maimemo.com/share/page?uid=22661175&pid=a23a5d11d1ef1192c62e0f26a34d6334&tid=6eac5f957d63f79fd9874d1d5a23d272",
-    "https://www.maimemo.com/share/page?uid=22661175&pid=a23a5d11d1ef1192c62e0f26a34d6334&tid=6eac5f957d63f79fd9874d1d5a23d272",
-    "https://www.maimemo.com/share/page?uid=22661175&pid=a23a5d11d1ef1192c62e0f26a34d6334&tid=6eac5f957d63f79fd9874d1d5a23d272",
+    "https://www.maimemo.com/share/page?uid=22661175&pid=c5ddaa52663cb88bf3920b964ff2b807&tid=94afe678e6c8628c6715593b7f9c6319",
+    "https://www.maimemo.com/share/page?uid=22661175&pid=c5ddaa52663cb88bf3920b964ff2b807&tid=94afe678e6c8628c6715593b7f9c6319",
+    "https://www.maimemo.com/share/page?uid=22661175&pid=c5ddaa52663cb88bf3920b964ff2b807&tid=94afe678e6c8628c6715593b7f9c6319",
   ]
 =====================以上为范例！=====================
 
@@ -20,9 +20,9 @@ const MMSLS = [
  */
 const MMSLS = [
     
-    "https://www.maimemo.com/share/page?uid=22661175&pid=a23a5d11d1ef1192c62e0f26a34d6334&tid=6eac5f957d63f79fd9874d1d5a23d272",
-    "https://www.maimemo.com/share/page?uid=22661175&pid=a23a5d11d1ef1192c62e0f26a34d6334&tid=6eac5f957d63f79fd9874d1d5a23d272",
-    "https://www.maimemo.com/share/page?uid=22661175&pid=a23a5d11d1ef1192c62e0f26a34d6334&tid=6eac5f957d63f79fd9874d1d5a23d272",
+    "https://www.maimemo.com/share/page?uid=22661175&pid=c5ddaa52663cb88bf3920b964ff2b807&tid=94afe678e6c8628c6715593b7f9c6319",
+    "https://www.maimemo.com/share/page?uid=22661175&pid=c5ddaa52663cb88bf3920b964ff2b807&tid=94afe678e6c8628c6715593b7f9c6319",
+    "https://www.maimemo.com/share/page?uid=22661175&pid=c5ddaa52663cb88bf3920b964ff2b807&tid=94afe678e6c8628c6715593b7f9c6319",
   ]
   /**
    * 生成随机数字
